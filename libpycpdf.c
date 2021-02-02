e7c0b332b8a20740a384f575f18bf3ca19e89486
John Whitington <john@coherentgraphics.co.uk>
2021-02-02 15:33:14 +0000

    more

--- libpycpdf.c
+++ libpycpdf.c
@@ -57,9 +57,33 @@ int pycpdf_fromFile(char *filename, char *userpw)
   return cpdf_fromFile(filename, userpw);
 }
 
+int pycpdf_fromFileLazy(char *filename, char *userpw)
+{
+  return cpdf_fromFileLazy(filename, userpw);
+}
+
+int pycpdf_fromMemory(void* data, int len, char *userpw)
+{
+  return cpdf_fromMemory(data, len, userpw);
+}
+
+int pycpdf_fromMemoryLazy(void* data, int len, char *userpw)
+{
+  return cpdf_fromMemoryLazy(data, len, userpw);
+}
+
+int pycpdf_blankDocument(double w, double h, int pages)
+{
+  return cpdf_blankDocument(w, h, pages);
+}
+
+int pycpdf_blankDocumentPaper(int papersize, int pages)
+{
+  return cpdf_blankDocumentPaper(papersize, pages);
+}
+
 void pycpdf_toFile(int pdf, char *filename, int linearize, int make_id)
 {
   cpdf_toFile(pdf, filename, linearize, make_id);
   return;
 }
-
--- pycpdf.py
+++ pycpdf.py
@@ -7,6 +7,7 @@ def loadDLL(f):
   libc = CDLL(f)
   libc.pycpdf_version.restype = POINTER(c_char)
   libc.pycpdf_lastErrorString.restype = POINTER(c_char)
+  libc.pycpdf_blankDocument.argtypes = [c_double, c_double, c_int]
 
 #CHAPTER 0. Preliminaries
 def startup():
@@ -37,6 +38,39 @@ def onExit():
 def fromFile(filename, userpw):
   return libc.pycpdf_fromFile(str.encode(filename), str.encode(userpw))
 
+def fromFileLazy(filename, userpw):
+  return libc.pycpdf_fromFileLazy(str.encode(filename), str.encode(userpw))
+
+def fromMemory(data, userpw):
+  return libc.pycpdf_fromMemory(data, len(data), str.encode(userpw))
+
+def fromMemoryLazy(data, userpw):
+  return libc.pycpdf_fromMemoryLazy(data, len(data), str.encode(userpw))
+
+def blankDocument(w, h, pages):
+  return libc.pycpdf_blankDocument(w, h, pages)
+
+a0portrait = 0
+a1portrait = 1
+a2portrait = 2
+a3portrait = 3
+a4portrait = 4
+a5portrait = 5
+a0landscape = 6
+a1landscape = 7
+a2landscape = 8
+a3landscape = 9
+a4landscape = 10
+a5landscape = 11
+usletterportrait = 12
+usletterlandscape = 13
+uslegalportrait = 14
+uslegallandscape = 15
+
+def blankDocumentPaper(papersize, pages):
+  return libc.pycpdf_blankDocumentPaper(papersize, pages)
+
 def toFile(pdf, filename, linearize, make_id):
   libc.pycpdf_toFile(pdf, str.encode(filename), False, False)
+
 
