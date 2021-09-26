4cb953c429fcf87779679d7da3e292404473102a
John Whitington <john@coherentgraphics.co.uk>
2021-09-26 16:20:48 +0100

    more

--- examples/merge.py
+++ examples/merge.py
@@ -1,27 +1,27 @@
 #Merge example
 import sys
 sys.path.insert(0,'..')
-import pycpdf
+import pycpdflib
 
 # DLL loading depends on your own platform. These are the author's settings.
 if sys.platform.startswith('darwin'):
-    pycpdf.loadDLL("/Users/john/repos/python-libcpdf/libpycpdf.so")
+    pycpdflib.loadDLL("/Users/john/repos/python-libcpdf/libpycpdf.so")
 elif sys.platform.startswith('linux'):
-    pycpdf.loadDLL("../libpycpdf.so")
+    pycpdflib.loadDLL("../libpycpdf.so")
 elif sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
     os.add_dll_directory("C:\\\\OCaml64/home/JohnWhitington/python-libcpdf/")
-    pycpdf.loadDLL("libpycpdf.dll")
+    pycpdflib.loadDLL("libpycpdf.dll")
 
-pycpdf.startup()
+pycpdflib.startup()
 
 #We will take the input hello.pdf and repeat it three times
-mergepdf = pycpdf.fromFile('../hello.pdf', '')
+mergepdf = pycpdflib.fromFile('../hello.pdf', '')
 
 #The list of PDFs to merge
 pdfs = [mergepdf, mergepdf, mergepdf]
 
 #Merge them
-merged = pycpdf.mergeSimple(pdfs)
+merged = pycpdflib.mergeSimple(pdfs)
 
 #Write output
-pycpdf.toFile(merged, 'merged.pdf', False, False)
+pycpdflib.toFile(merged, 'merged.pdf', False, False)
--- examples/squeeze.py
+++ examples/squeeze.py
@@ -1,25 +1,25 @@
 #Squeeze example
 import sys
 sys.path.insert(0,'..')
-import pycpdf
+import pycpdflib
 
 #DLL loading depends on your own platform. These are the author's settings.
 if sys.platform.startswith('darwin'):
-    pycpdf.loadDLL("/Users/john/repos/python-libcpdf/libpycpdf.so")
+    pycpdflib.loadDLL("/Users/john/repos/python-libcpdf/libpycpdf.so")
 elif sys.platform.startswith('linux'):
-    pycpdf.loadDLL("../libpycpdf.so")
+    pycpdflib.loadDLL("../libpycpdf.so")
 elif sys.platform.startswith('win32') or sys.platform.startswith('cygwin'):
     os.add_dll_directory("C:\\\\OCaml64/home/JohnWhitington/python-libcpdf/")
-    pycpdf.loadDLL("libpycpdf.dll")
+    pycpdflib.loadDLL("libpycpdf.dll")
 
 #Prepare cpdf for use
-pycpdf.startup()
+pycpdflib.startup()
 
 #Load file
-pdf = pycpdf.fromFile('../cpdflibmanual.pdf', '')
+pdf = pycpdflib.fromFile('../cpdflibmanual.pdf', '')
 
 #Squeeze it
-pycpdf.squeezeInMemory(pdf)
+pycpdflib.squeezeInMemory(pdf)
 
 #Write output. We make sure to use toFileExt, and make object streams.
-pycpdf.toFileExt(pdf, 'squeezed.pdf', False, False, True, True, True)
+pycpdflib.toFileExt(pdf, 'squeezed.pdf', False, False, True, True, True)
